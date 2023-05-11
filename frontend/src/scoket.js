fb46688130df6be3b2a067b11f53f06f44b62faf
Nitish Kumar Gupta <57590993+nitishgupta08@users.noreply.github.com>
2023-05-12 01:33:11 +0530

    minor bug fixes

--- frontend/src/scoket.js
+++ frontend/src/scoket.js
@@ -1,10 +1,10 @@
 import { io } from "socket.io-client";
 
 const options = {
-  // reconnect_attempt: "Infinity",
+  reconnect_attempt: "Infinity",
   // timeout: 10000,
   autoConnect: false,
-  // transports: ["websocket"],
+  transports: ["websocket"],
 };
 
 export const socket = io(process.env.REACT_APP_BACKEND_URL, options);
